05b337b9b965bb3e8d6b39778dd3ba9e9988a095
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-15 15:50:52 -0400

    fixed /urls/new route position

--- express_server.js
+++ express_server.js
@@ -18,16 +18,16 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     longURL: urlDatabase[req.params.shortURL],
     shortURL: req.params.shortURL,
   };
   res.render('urls_show', templateVars);
-});
-
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
 });
 
 app.get('/urls.json', (req, res) => {
