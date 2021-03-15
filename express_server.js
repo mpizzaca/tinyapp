2c71115a7bd8863b8c6f21216bece1b9e2b3e3d1
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-15 15:32:02 -0400

    linting

--- express_server.js
+++ express_server.js
@@ -19,9 +19,9 @@ app.get('/urls', (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     longURL: urlDatabase[req.params.shortURL],
-    shortURL: req.params.shortURL,  
+    shortURL: req.params.shortURL,
   };
   res.render('urls_show', templateVars);
 });
